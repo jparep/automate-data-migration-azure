ca109e1e1db40bde61a73dd9ea080e0f6ade2d71
jparep <parepson@gmail.com>
2025-01-25 16:35:57 -0700

    Retrieve connection string from env file

--- migration.py
+++ migration.py
@@ -5,3 +5,6 @@ from azure.storage.blob import BlobServiceClient
 
 # Load environment variables from .env file
 load_dotenv()
+
+# Retrive connection string from environment variable
+connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
