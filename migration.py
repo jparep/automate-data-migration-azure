557734b2376bf5418fc31f853a6a7c7505cf7324
jparep <parepson@gmail.com>
2025-01-25 16:36:56 -0700

    Create Blog Service Client

--- migration.py
+++ migration.py
@@ -8,3 +8,6 @@ load_dotenv()
 
 # Retrive connection string from environment variable
 connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+
+# Use the connection string to create a BlobServiceClient
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
